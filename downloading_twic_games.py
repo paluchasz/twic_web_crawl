9ad70bc236953a572d387eb0e7c437c32e68362e
paluchasz <paluchasz@googlemail.com>
2018-10-26 16:31:27 +0100

    Just a few more comments and changed the path as I renamed the folder.

--- downloading_twic_games.py
+++ downloading_twic_games.py
@@ -11,22 +11,23 @@ c = pycurl.Curl()
 for i in range(1250, 1251):
     # notice the url is without 'www', this is beacuse the url has changed, I was getting a 301 error previously
     url = "http://theweekinchess.com/zips/twic" + str(i) + "g.zip"
-    save_location_path = Path("/home/paluchasz/Desktop/chess_base_stuff/downloading_twic/twic" + str(i) + ".zip")
-    unzipped_location_path = Path("/home/paluchasz/Desktop/chess_base_stuff/downloading_twic")
+    save_location_path = Path("/home/paluchasz/Desktop/chessbase_stuff/downloading_twic/twic" + str(i) + ".zip")
+    unzipped_location_path = Path("/home/paluchasz/Desktop/chessbase_stuff/downloading_twic")
     # reason for using Path module is that now this code can be run on Windows as well where the paths use \ instead
 
-    # downloading the file
+    # downloading the file - taken from pycurl documentation
     with open(save_location_path, 'wb') as f:
 
         c.setopt(c.URL, url)
         c.setopt(c.WRITEDATA, f)
         c.perform()
 
-    # unziping the file
+    # unziping the file - first line reads it (hence the 'r'), second line extracts it
     with zipfile.ZipFile(save_location_path, 'r') as myzip:
         myzip.extractall(unzipped_location_path)
 
     # removing old zip file
     os.remove(save_location_path)
 
+# close Curl object
 c.close()
