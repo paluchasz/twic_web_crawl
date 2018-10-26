ef54ee668b443cfb644c3145f5cd18b8a4717bb3
paluchasz <paluchasz@googlemail.com>
2018-10-26 15:06:57 +0100

    Now downloading pgn instead of cbv format
    I realised that it doesn't matter which I download as both can be opened in chessbase and copied to the twic database easily. The difference
    with pgn is that I first have to convert it to cbv format in the chassbase interface. The reason for using pgn instead is because the files
    can be concatinated into a single file with the cat command (Using $ cat * >twic1143-1250.pgn in terminal to be specific). It is not
    possible to do this with the cbv files. This way I only have to append to the Twic database once!

--- practice_pycurl.py
+++ practice_pycurl.py
@@ -3,22 +3,28 @@ from io import BytesIO
 import os
 import zipfile
 
+# Initialise Curl object
 c = pycurl.Curl()
 
-for i in range(1140, 1251):
-    url = "http://theweekinchess.com/zips/twic" + str(i) + "c6.zip"
-    path_to_save_location = "downloading_twic/twic" + str(i) + ".zip"
-    unzipped_location = "downloading_twic"
+# change the range depending on which twic files I need to download
+for i in range(1143, 1251):
+    # notice the url is without 'www', this is beacuse the url has changed, I was getting a 301 error previously
+    url = "http://theweekinchess.com/zips/twic" + str(i) + "g.zip"
+    save_location_path = "/home/paluchasz/Desktop/chess_base_stuff/downloading_twic/twic" + str(i) + ".zip"
+    unzipped_location_path = "/home/paluchasz/Desktop/chess_base_stuff/downloading_twic"
 
-    with open(path_to_save_location, 'wb') as f:
+    # downloading the file
+    with open(save_location_path, 'wb') as f:
 
         c.setopt(c.URL, url)
         c.setopt(c.WRITEDATA, f)
         c.perform()
 
-    with zipfile.ZipFile(path_to_save_location, 'r') as myzip:
-        myzip.extractall(unzipped_location)
+    # unziping the file
+    with zipfile.ZipFile(save_location_path, 'r') as myzip:
+        myzip.extractall(unzipped_location_path)
 
-    os.remove(path_to_save_location)
+    # removing old zip file
+    os.remove(save_location_path)
 
 c.close()
