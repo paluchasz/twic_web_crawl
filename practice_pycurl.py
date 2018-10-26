f24089e7bc11937bb2be910237c21c6f50311c49
paluchasz <paluchasz@googlemail.com>
2018-10-26 12:29:08 +0100

    Added unzipping feature.
    By using the zipfile library I managed to unzip all the files as well.

--- practice_pycurl.py
+++ practice_pycurl.py
@@ -8,11 +8,15 @@ c = pycurl.Curl()
 for i in range(1140, 1251):
     url = "http://theweekinchess.com/zips/twic" + str(i) + "c6.zip"
     path_to_save_location = "downloading_twic/twic" + str(i) + ".zip"
+    unzipped_location = "downloading_twic"
 
     with open(path_to_save_location, 'wb') as f:
 
         c.setopt(c.URL, url)
         c.setopt(c.WRITEDATA, f)
         c.perform()
+
+    with zipfile.ZipFile(path_to_save_location, 'r') as myzip:
+        myzip.extractall(unzipped_location)
 
 c.close()
