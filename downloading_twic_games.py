bc3910dd3f3b343d8649ee050d40989f487b8c35
paluchasz <paluchasz@googlemail.com>
2018-10-26 15:53:15 +0100

    Using Path module form pathlib so that the code can be run on any operating systems
    So if it is now run on windows the Path module would change the forward slashes (/) in the path to backslashes (\) which is what windows
    paths use.

--- downloading_twic_games.py
+++ downloading_twic_games.py
@@ -2,16 +2,18 @@ import pycurl
 from io import BytesIO
 import os
 import zipfile
+from pathlib import Path
 
 # Initialise Curl object
 c = pycurl.Curl()
 
 # change the range depending on which twic files I need to download
-for i in range(1143, 1251):
+for i in range(1250, 1251):
     # notice the url is without 'www', this is beacuse the url has changed, I was getting a 301 error previously
     url = "http://theweekinchess.com/zips/twic" + str(i) + "g.zip"
-    save_location_path = "/home/paluchasz/Desktop/chess_base_stuff/downloading_twic/twic" + str(i) + ".zip"
-    unzipped_location_path = "/home/paluchasz/Desktop/chess_base_stuff/downloading_twic"
+    save_location_path = Path("/home/paluchasz/Desktop/chess_base_stuff/downloading_twic/twic" + str(i) + ".zip")
+    unzipped_location_path = Path("/home/paluchasz/Desktop/chess_base_stuff/downloading_twic")
+    # reason for using Path module is that now this code can be run on Windows as well where the paths use \ instead
 
     # downloading the file
     with open(save_location_path, 'wb') as f:
